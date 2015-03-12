98f5e4bf33271952edb8140acf7237ec12a02355
Igor Freire <igor.auad@gmail.com>
2015-03-12 16:39:25 -0300

    Add swig as template engine and structure for backend controllers/views

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var swig  = require('swig');
+var controller = require('./app/controllers/core.server.controller.js')
+
+// Configure Swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/app/views');
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.get('/', controller.home);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
